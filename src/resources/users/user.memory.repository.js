7148abec55553c7d8425f32298694b42edc8acd7
Vlad Meleshko <vladik4.98@mail.ru>
2021-05-14 10:41:53 +0300

    feat: add methods for /users route

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,36 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
-};
+const User = require('./user.model');
 
-module.exports = { getAll };
+const users = [new User({name: 'user', login: 'user', password: 'user'})];
+
+const getAll = async () => users;
+
+const getUserById = async (id) => users.find(user => user.id === id);
+
+const createUser = async (name, login, password) => {
+  const newUser = new User({name, login, password});
+  users.push(newUser);
+  return newUser;
+}
+
+const updateUser = async (id, name, login, password) => {
+  const userPresence = users.findIndex(user => user.id === id);
+  if (userPresence === -1) {
+    throw new Error('User is not exist!');
+  } else {
+    users[userPresence].name = name;
+    users[userPresence].login = login;
+    users[userPresence].password = password;
+    return users[userPresence];
+  }
+}
+
+const deleteUser = async (id) => {
+  const userPresence = users.findIndex(user => user.id === id);
+  if (userPresence === -1) {
+    throw new Error('User is not exist!');
+  } else {
+    users.filter(user => user.id !== id);
+  }
+}
+
+module.exports = { getAll, getUserById, createUser, updateUser, deleteUser };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -2,4 +2,12 @@ const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const getUserById = (id) => usersRepo.getUserById(id);
+
+const createUser = (name, login, password) => usersRepo.createUser(name, login, password);
+
+const updateUser = (id, name, login, password) => usersRepo.updateUser(id, name, login, password);
+
+const deleteUser = (id) => usersRepo.deleteUser(id);
+
+module.exports = { getAll, getUserById, createUser, updateUser, deleteUser };
