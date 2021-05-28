b13a6390f9ec0cadf1df358cca57da16e7aae56c
Vlad Meleshko <vladik4.98@mail.ru>
2021-05-28 13:43:25 +0300

    docs: add users typing information

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,6 +1,30 @@
 const {v4: uuid} = require('uuid');
 
+/**
+ * @typedef {Object} UserInfo user's name and login.
+ * @property {string} name user name.
+ * @property {string} login user login.
+ */
+
+/**
+ * @typedef {UserInfo & {password: string}} UserCredentials contains user's credentials (name, login, password).
+ */
+
+/**
+ * @typedef {UserInfo & {id: string}} UserAccount contains account parameters (id, name, login).
+ */
+
+/**
+ * @typedef {UserCredentials & {id: string}} UserModel contains full user's information (id, name, login, password).
+ */
+
+/** Class representing a user. */
 class User {
+  /**
+   * Create a user.
+   * @param {UserCredentials} newUser parameters of new user.
+   * @returns {UserModel} new user.
+   */
   constructor({
     id = uuid(),
     name = 'USER',
@@ -13,6 +37,11 @@ class User {
     this.password = password;
   }
 
+  /**
+   * Processes user information from the server.
+   * @param {UserModel} user user's information.
+   * @returns {UserAccount} returns account information about user.
+   */
   static toResponse(user) {
     const { id, name, login } = user;
     return { id, name, login };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,13 +1,51 @@
 const usersRepo = require('./user.memory.repository');
 const tasksRepo = require('../tasks/task.memory.repository');
 
+/**
+ * @typedef {import('./user.model').UserAccount} UserAccount user's id, name, login.
+ */
+
+/**
+ * Returns information about all users from the database using the controller function.
+ * @returns {Promise<Array.<UserAccount>>} resolved promise with users and their information.
+ */
+
 const getAll = () => usersRepo.getAll();
+
+/**
+ * Returns information about selected user from the database using the controller function.
+ * @param {string} id id of selected user.
+ * @returns {Promise<UserAccount>} resolved promise with selected user and his information.
+ */
 
 const getUserById = (id) => usersRepo.getUserById(id);
 
+/**
+ * Create new user and his information in the database using the controller function.
+ * @param {string} name name of new user.
+ * @param {string} login login of new user.
+ * @param {string} password password of new user.
+ * @returns {Promise<UserAccount>} resolved promise with new user and his information.
+ */
+
 const createUser = (name, login, password) => usersRepo.createUser(name, login, password);
 
+/**
+ * Update user's information in the database using the controller function.
+ * @param {string} id id of user, whose information will be update.
+ * @param {string} name new name of selected user.
+ * @param {string} login new login of selected user.
+ * @param {string} password new password of selected user.
+ * @returns {Promise<UserAccount>} resolved promise with user and his new information or an error if user is not found.
+ */
+
 const updateUser = (id, name, login, password) => usersRepo.updateUser(id, name, login, password);
+
+/**
+ * Delete user and all tasks where user was assigned updated to put user's id to null.
+ * @param {string} id id of deleted user.
+ * @returns {void} return an error or nothing.
+ */
 
 const deleteUser = (id) => {
     usersRepo.deleteUser(id);
