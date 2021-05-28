997191bfac051ac1d8ec5148e9310c8018651b58
Vlad Meleshko <vladik4.98@mail.ru>
2021-05-28 22:07:54 +0300

    docs: add boards typing information

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -1,6 +1,29 @@
 const {v4: uuid} = require('uuid');
 
+/**
+ * @typedef {Object} ColumnsModel columns information.
+ * @property {string} id columns id.
+ * @property {string} title columns title.
+ * @property {number} order columns order.
+ */
+
+/**
+ * @typedef {Object} BoardInfo board's information.
+ * @property {string} title board title.
+ * @property {Array.<ColumnsModel>} columns board columns.
+ */
+
+/**
+ * @typedef {BoardInfo & {id: string}} BoardModel contains board's information and board's id.
+ */
+
+/** Class representing a board. */
 class Board {
+  /**
+   * Create a board.
+   * @param {BoardInfo} newBoard parameters of new board.
+   * @returns {BoardModel} new board.
+   */
     constructor({
       id = uuid(),
       title = 'TITLE',
@@ -10,7 +33,12 @@ class Board {
       this.title = title;
       this.columns = columns;
     }
-  
+
+    /**
+     * Processes board information from the server.
+     * @param {BoardModel} board board's information from server.
+     * @returns {BoardModel} return processed board information.
+     */  
     static toResponse(board) {
       const { id, title, columns } = board;
       return { id, title, columns };
--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,13 +1,50 @@
 const boardsRepo = require('./board.memory.repository');
 const tasksRepo = require('../tasks/task.memory.repository');
 
+/**
+ * @typedef {import('./board.model').BoardModel} BoardModel
+ * @typedef {import('./board.model').ColumnsModel} ColumnsModel
+ */
+
+/**
+ * Returns information about all boards from the database using the controller function.
+ * @returns {Promise<Array.<BoardModel>>} resolved promise with boards and their information.
+ */
+
 const getAll = () => boardsRepo.getAll();
+
+/**
+ * Returns information about selected board from the database using the controller function.
+ * @param {string} id id of selected board.
+ * @returns {Promise<BoardModel>} resolved promise with selected board and its information.
+ */
 
 const getBoardById = (id) => boardsRepo.getBoardById(id);
 
+/**
+ * Create new board and its information in the database using the controller function.
+ * @param {string} title title of new board.
+ * @param {Array.<ColumnsModel>} columns columns of new board.
+ * @returns {Promise<BoardModel>} resolved promise with new board and its information.
+ */
+
 const createBoard = (title, columns) => boardsRepo.createBoard(title, columns);
 
+/**
+ * Update board's information in the database using the controller function.
+ * @param {string} id id of board, which information will be update.
+ * @param {string} title new title of selected board.
+ * @param {Array.<ColumnsModel>} columns new columns of selected board.
+ * @returns {Promise<BoardModel>} resolved promise with board and its new information or an error if board is not found.
+ */
+
 const updateBoard = (id, title, columns) => boardsRepo.updateBoard(id, title, columns);
+
+/**
+ * Delete board and its information from the database and all tasks that were in it using the controller functions.
+ * @param {string} id id of deleted board.
+ * @returns {void} return an error or nothing.
+ */
 
 const deleteBoard = (id) => {
     boardsRepo.deleteBoard(id);
