a797b58fa1e8ce778511d3726985fa6f8347915c
Vlad Meleshko <vladik4.98@mail.ru>
2021-05-28 16:14:13 +0300

    docs: add tasks typing information

--- src/resources/tasks/task.model.js
+++ src/resources/tasks/task.model.js
@@ -1,6 +1,26 @@
 const {v4: uuid} = require('uuid');
 
+/**
+ * @typedef {Object} TaskInfo task's information.
+ * @property {string} title task title
+ * @property {number} order task order
+ * @property {string} description task description
+ * @property {string|null} userId task userId
+ * @property {string} boardId task boardId
+ * @property {string} columnId task columnId
+ */
+
+/**
+ * @typedef {TaskInfo & {id: string}} TaskModel contains task's information and task's id.
+ */
+
+/** Class representing a task. */
 class Task {
+    /**
+     * Create a task.
+     * @param {TaskInfo} newTask parameters of new task.
+     * @returns {TaskModel} new task.
+     */
     constructor({
       id = uuid(),
       title = 'TITLE',
@@ -19,6 +39,11 @@ class Task {
       this.columnId = columnId;
     }
   
+    /**
+     * Processes task information from the server.
+     * @param {TaskModel} task task's information from server.
+     * @returns {TaskModel} return processed information about task.
+     */
     static toResponse(task) {
       const { id, title, order, description, userId, boardId, columnId } = task;
       return { id, title, order, description, userId, boardId, columnId };
